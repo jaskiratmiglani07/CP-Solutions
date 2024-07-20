58b3e330c3a0d0eaeb89eba7cdc2741a7c6b1f2d
jaskiratmiglani07 <162829654+jaskiratmiglani07@users.noreply.github.com>
2024-07-21 01:58:04 +0530

    Committing files

---
+++ CodeForces/C++20 (GCC 13-64)/231A | Team/271511839.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n][3];
+    int ans=0;
+    for(int i=0; i<n; i++)
+    {
+       for(int j=0; j<3; j++)
+       {
+        cin>>arr[i][j];
+       }
+    }
+    for(int i=0; i<n; i++)
+    {
+        int count=0;
+        for(int j=0; j<3; j++)
+        {
+            if(arr[i][j]==1)
+            count++;
+        }
+        if(count>=2)
+        ans++;
+    }
+    cout<<ans;
+}
